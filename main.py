2784ca6f21143a53d8ca453b9cb997c392bcef05
zagarak <168479795+zagarak@users.noreply.github.com>
2026-06-23 14:27:06 -0500

    Update main
    Added a loader for AutoSecurityBox with comments on future module integration and current limitations.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-##
+# ASB Loader
+# Eventually asb, asb_fman, and asb_hasher modules will move to firmwareland.
+# This loader will be the entry point into asb to protect keys.json stored hash from
+# accidental exposure. I am currently having trouble building Micropython with frozen 
+# modules and FS support is not currently working. However, everything works as normal
+# for now with modules in /lib on prebuilt Micropython.
+
+def load_asb():
+    print("Loading AutoSecurityBox...")
+    import asb
+
+if __name__ == "__main__":
+    load_asb()
