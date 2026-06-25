8e41393ac7075a7a9c06943556fa12d1e550d4f4
zagarak <168479795+zagarak@users.noreply.github.com>
2026-06-24 19:25:30 -0500

    Update manifest.py with modules
    Added comments and module declarations for MicroPython RPI_PICO module manifest.

--- firmware/ports/rp2/manifest.py
+++ firmware/ports/rp2/manifest.py
@@ -1,1 +1,12 @@
-
+# manifest.py
+# Place manifest.py in ports/rp2 of your micropython repo.
+# Then add asb modules to ports/rp2/module and build with manifest.
+# If you don't include _boot, _boot_fat, and rp2 you will not have filesystem access!
+# Freeze board libraries and asb modules.
+module("_boot.py")
+module("_boot_fat.py")
+module("rp2.py")
+module("mfrc522.py")
+module("asb.py")
+module("asb_fman.py")
+module("asb_hasher.py")
